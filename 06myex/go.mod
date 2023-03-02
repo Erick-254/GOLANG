f60ec36d0323c5b20a792038ef3d32e6f90c5e19
Erick-254 <erickrotich768@gmail.com>
2023-03-02 09:45:52 +0100

    myex

---
+++ 06myex/go.mod
@@ -0,0 +1,3 @@
+module myexp
+
+go 1.19
---
+++ 06myex/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var example = 7
+	fmt.Println("my example is:")
+	fmt.Printf("My example is of variable type:%T", example)
+
+}
