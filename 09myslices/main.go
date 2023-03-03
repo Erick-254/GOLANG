baf1e0a358d4dbe4c53b75dc637c1158f7792d7b
Erick-254 <erickrotich768@gmail.com>
2023-03-03 19:43:07 +0100

    added sort

--- 09myslices/main.go
+++ 09myslices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Welcome to videos on slices")
@@ -12,7 +15,22 @@ func main() {
 
 	fmt.Println(fruitList)
 
-	fruitList = append(fruitList[1:3])
+	fruitList = append(fruitList[1:])
 	fmt.Println(fruitList)
+
+	highScore := make([]int, 4)
+
+	highScore[0] = 934
+	highScore[1] = 334
+	highScore[2] = 434
+	highScore[3] = 934
+
+	highScore = append(highScore, 555, 666, 777, 888, 999) // apppend can accomodate all values
+
+	fmt.Println(highScore)
+	//only available in slice
+	sort.Ints(highScore)
+	fmt.Println(highScore)
+	fmt.Println(sort.IntsAreSorted(highScore))
 
 }
